3c65dd311ab37e65c7c5eb848cdcacdc19af6ae9
Maguzzz <markusm.touray@gmail.com>
2023-12-04 12:50:06 +0100

    Insert user into db

--- index.php
+++ index.php
@@ -8,17 +8,42 @@
 </head>
 
 <body>
-    <?php
-session_start();
-        $conn = mysqli_connect("localhost", "root", "", "review");
-
-        if(!$conn) {
-            echo "Connection failed <br>";
-        }
-        echo "Connected successfully <br>";
-    ?>
-
+    <form action="index.php" method="post">
+        <label>name:</label>
+        <input type="text" name="name"><br>
+        <label>review:</label>
+        <input type="text" name="review"><br>
+        <input type="submit" value="POST">
+    </form>
 
 </body>
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "review");
+
+$userName = $_POST["name"];
+$userReview = $_POST["review"];
+$userRating = 5;
+
+if (!$conn) {
+    echo "[Connection failed]<br>";
+}
+
+echo "Connected successfully <br>";
+
+if (!($userName != null && $userReview != null)) {
+    echo "[pls fill out the whole formula]";
+} else {
+    $sql = "INSERT INTO tb_reviews (userName, review, rating) VALUES ('$userName', '$userReview ', '$userRating')";
+
+    if ($conn->query($sql) === true) {
+        echo "[New review created successfully]";
+
+    } else {
+        echo "[Error creating review]";
+    }
+}
+
+?>
 
 </html>
